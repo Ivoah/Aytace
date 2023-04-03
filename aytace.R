7dce52d794bf2cd055b9558f5ce6c13e817771a9
Noah Rosamilia <noah@ivoah.net>
2023-04-03 16:49:21 -0400

    Antialiasing

--- aytace.R
+++ aytace.R
@@ -2,6 +2,7 @@ source("vec.R")
 source("ray.R")
 source("color.R")
 source("hittable.R")
+source("camera.R")
 
 # Image
 aspect_ratio <- 16.0 / 9.0
@@ -12,6 +13,7 @@ image_height <- image_width / aspect_ratio
 viewport_height <- 2.0
 viewport_width <- aspect_ratio * viewport_height
 focal_length <- 1.0
+samples <- 25
 
 origin <- c(0, 0, 0)
 horizontal <- c(viewport_width, 0, 0)
@@ -28,20 +30,24 @@ world <- hittable_list(
 ray_color <- function(ray) {
   hit <- world(ray, 0, Inf)
   if (!is.null(hit <- world(ray, 0, Inf))) {
-      to_rgb(0.5 * (hit$normal + c(1, 1, 1)))
+      0.5 * (hit$normal + c(1, 1, 1))
   } else {
     unit_direction <- vec_unit(ray$dir)
     t <- 0.5*(unit_direction[2] + 1.0)
-    to_rgb((1.0-t)*c(1.0, 1.0, 1.0) + t*c(0.5, 0.7, 1.0))
+    (1.0-t)*c(1.0, 1.0, 1.0) + t*c(0.5, 0.7, 1.0)
   }
 }
 
 for (j in seq(image_height-1, 0, -1)) {
   cat("\rScanlines remaining:", j, "")
   for (i in 0:(image_width-1)) {
-    u <- i / (image_width-1)
-    v <- j / (image_height-1)
-    img[image_height - 1 - j,i] <- ray_color(new_ray(origin, lower_left_corner + u*horizontal + v*vertical - origin))
+    pixel <- c(0, 0, 0)
+    for (s in 1:samples) {
+      u <- (i + runif(1)) / (image_width-1)
+      v <- (j + runif(1)) / (image_height-1)
+      pixel <- pixel + ray_color(new_ray(origin, lower_left_corner + u*horizontal + v*vertical - origin))
+    }
+    img[image_height - 1 - j,i] <- to_rgb(pixel/samples)
   }
 }
 
